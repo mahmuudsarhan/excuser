a1bb13693f80731462cc133e0883b28c03304100
! <mahmuudsarhan@gmail.com>
2026-06-01 21:43:21 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+const axios = require("axios");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/exc-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/exc-joke", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({
+      text:
+`${response.data.setup}
+
+${response.data.punchline}`
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
+
+
+
+app.command("/exc-help", async ({ command, ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/exc-ping - Check bot latency
+/exc-catfact - Get a cat fact
+/exc-joke - Joke!`
+  });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
